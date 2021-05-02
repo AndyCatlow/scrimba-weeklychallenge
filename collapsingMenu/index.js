301e54df5c4b40b232ba3dbaeacd9e6142249862
AndyCatlow <mrcatlow@gmail.com>
2021-05-02 21:29:14 +0200

    refactor nav toggle to use a function

--- collapsingMenu/index.js
+++ collapsingMenu/index.js
@@ -1,21 +1,23 @@
 const openArrow = document.querySelector(".open-menu");
+const closeArrow = document.querySelector(".close-menu");
 const container = document.querySelector(".container");
 const nav = document.querySelector(".nav");
-const closeArrow = document.querySelector(".close-menu");
 const llama = document.getElementById("llama");
 
-openArrow.addEventListener("click", function (e) {
-  container.classList.toggle("inactive-container");
-  nav.classList.toggle("active-nav");
+openArrow.addEventListener("click", () => {
+  toggleNav();
 });
 
-closeArrow.addEventListener("click", function (e) {
+closeArrow.addEventListener("click", () => {
+  toggleNav();
+});
+
+let toggleNav = () => {
   container.classList.toggle("inactive-container");
   nav.classList.toggle("active-nav");
-});
+};
 
 llama.addEventListener("click", function (e) {
   let random = Math.floor(Math.random() * 10);
   llama.src = `./images/${random}.jpg`;
-  console.log(llama.src);
 });
