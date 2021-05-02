01e9091bfd1218983b4d1fa17c4bb5d76c36e1dd
AndyCatlow <mrcatlow@gmail.com>
2021-05-02 20:29:08 +0200

    add llama random picture function

--- collapsingMenu/index.js
+++ collapsingMenu/index.js
@@ -2,6 +2,7 @@ const openArrow = document.querySelector(".open-menu");
 const container = document.querySelector(".container");
 const nav = document.querySelector(".nav");
 const closeArrow = document.querySelector(".close-menu");
+const llama = document.getElementById("llama");
 
 openArrow.addEventListener("click", function (e) {
   container.classList.toggle("inactive-container");
@@ -11,4 +12,10 @@ openArrow.addEventListener("click", function (e) {
 closeArrow.addEventListener("click", function (e) {
   container.classList.toggle("inactive-container");
   nav.classList.toggle("active-nav");
+});
+
+llama.addEventListener("click", function (e) {
+  let random = Math.floor(Math.random() * 10);
+  llama.src = `./images/${random}.jpg`;
+  console.log(llama.src);
 });
